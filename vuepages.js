194f65996370e08e1789c2940e35da73a9e61f26
loz <lauriej1316@gmail.com>
2019-01-28 07:23:32 +0000

    page-section paragraph array

--- vuepages.js
+++ vuepages.js
@@ -5,13 +5,13 @@ var vuepages = vuepages || {};
 	if(typeof window.Vue === 'undefined') throw new Error('Vue not present on window :(');
 	
 	Vue.component('page-section', {
-		props: ['title', 'content'],
+		props: ['title', 'paragraphs'],
 		data: function() { return {} },
 		methods: {},
 		template:
 			'<div class="section">\
 				  <h2>{{ title }}</h2>\
-				  <p>{{ content }}</p>\
+				  <p v-for="paragraph in paragraphs">{{ paragraph }}</p>\
 				  <slot></slot>\
 				</div>\
 				'
@@ -26,14 +26,14 @@ var vuepages = vuepages || {};
 			<div class="container">\
 			\
 			  <div class="header">\
-				<div class="image-container">\
+				<div class="image-container" v-show="headerimage">\
 					<img class="header-image" v-bind:src="headerimage" id="headerImage">\
 				</div>\
 				<h1>{{ title }}</h1>\
 			  </div>\
 			  \
 			  <div class="main">\
-				<page-section v-for="section in sections" v-bind:title="section.title" v-bind:content="section.content">{{section.slot ? section.slot : \'\'}}</page-section>\
+				<page-section v-for="section in sections" v-bind:title="section.title" v-bind:paragraphs="section.paragraphs">{{section.slot ? section.slot : \'\'}}</page-section>\
 				<slot></slot>\
 				<page-section v-bind:title="\'contact\'">\
 				  <p>\
